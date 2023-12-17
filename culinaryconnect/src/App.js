2b91b4e6b592ad68bf0f2c837941749651fdc36d
John-thabuks <thabuksjohn@gmail.com>
2023-12-17 22:20:39 +0300

    Styled components populated

--- culinaryconnect/src/App.js
+++ culinaryconnect/src/App.js
@@ -22,3 +22,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  
+`
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-item: center
+  svg{
+    font-size:2rem;
+  }
+`
+
+export default App;
