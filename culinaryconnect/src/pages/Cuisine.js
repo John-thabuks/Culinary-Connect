a9d85cc35b0a5aa3e6fa106f761ba4e95fc171bd
Ndindairene <irene.ndindaa@gmail.com>
2023-12-17 20:48:07 +0300

    Cuisine component

--- culinaryconnect/src/pages/Cuisine.js
+++ culinaryconnect/src/pages/Cuisine.js
@@ -1,60 +1,48 @@
-import React, {useState, useEffect} from 'react'
-import styled from "styled-components";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/splide/dist/css/splide.min.css';
-import { Link } from 'react-router-dom';
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import {motion} from "framer-motion"
+import { Link, useParams } from 'react-router-dom'
 
-function Veggies() {
 
-    const [veggie, setVeggies] = useState([]);
+function Cuisine() {
 
-    useEffect(() => {
-      const check = localStorage.getItem("veggie");
+    const [cuisine, setCuisine] = useState([])
+    let params = useParams()
+
+    const fetchCuisine = async(name) =>{
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ebbb33ca04e54326a40256cc799992b8&cuisine=${name}`)
+        
+        const recipes = await data.json()
+        setCuisine(recipes.results)
+    }
+
+    useEffect(()=>{
+        fetchCuisine(params.type)
+        console.log(params.type)
+    },[params.type])
+
+    
+
   
-      if (check) {
-        setVeggies(JSON.parse(check));
-      } else {
-        fetch(
-          `https://api.spoonacular.com/recipes/random?apiKey=ebbb33ca04e54326a40256cc799992b8&number=9&tags=vegetarian`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-             
-            localStorage.setItem("veggie", JSON.stringify(data.recipes));
-            setVeggies(data.recipes);
-          })
-          .catch((error) => console.error('Error fetching recipes:', error));
-      }
-    }, []);
-
 
     return (
-        <>
-      <Wrapper>
-        <h3 className='vegetarianPicks'>Vegetarian Picks</h3>
-        <Splide
-          options={{
-            perPage: 4,
-            arrows: false,
-            pagination: false,
-            drag: 'free',
-            gap: '5rem',
-          }}
+        <Grid
+            animate={{opacity:1}}
+            initial={{opacity:0}}
+            exit={{opacity:0}}
+            transition={{duration: 0.5}}
         >
-          {veggie &&
-            veggie.map((recipe) => (
-              <SplideSlide key={recipe.id}>
-                <Card>
-                  <Link to={"/recipe/"+recipe.id}>
-                    <p>{recipe.title}</p>
-                    <img src={recipe.image} alt={recipe.title} />
-                    <Gradient />
-                  </Link>
+            {cuisine && cuisine.map((item) => (
+                <Card key={item.id}>
+                    <Link to={'/recipe/'+item.id}>
+                        <img src={item.image} alt={item.title} />
+                        <h4>{item.title}</h4>
+                    </Link>
                 </Card>
-              </SplideSlide>
             ))}
-        </Splide>
-      </Wrapper>
-    </>
-    )
+        </Grid>
+    );
+    
 }
+
+export default Cuisine
