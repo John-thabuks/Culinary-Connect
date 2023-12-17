515cbf8782c0430f3ad957a4d1e715295fce2507
John-thabuks <thabuksjohn@gmail.com>
2023-12-17 22:12:59 +0300

    Styled component - Grid (Searched)

--- culinaryconnect/src/pages/Searched.js
+++ culinaryconnect/src/pages/Searched.js
@@ -36,6 +36,10 @@ function Searched() {
   )
 }
 
-
+const Grid = styled.div`
+    display:grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-gap: 3rem;
+`
 
 export default Searched
