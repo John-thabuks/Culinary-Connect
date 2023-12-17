05b6365435f317ebd6cbf7a4599d527bf7bd47c6
John-thabuks <thabuksjohn@gmail.com>
2023-12-17 22:18:41 +0300

    App: App component uploaded

--- culinaryconnect/src/App.js
+++ culinaryconnect/src/App.js
@@ -1,9 +1,24 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category"
+import {BrowserRouter} from "react-router-dom"
+import Search from "./components/Search"
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={'/'}>Culinary-Connect</Logo>
+      </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
